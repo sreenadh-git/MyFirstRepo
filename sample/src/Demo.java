6ca4971019f76bf06418794fd9846999e9614f28
sreenadhchowdary <sreenadhchowdary@LAPTOP-0USD9F0J>
2021-12-01 20:16:20 +0530

    hi guys

--- sample/src/Demo.java
+++ sample/src/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("a new demo class");
+		System.out.println("hi guys");
 	}
 
 }
