c3e970f15e69b6b79eafe24b849a15153f64f619
sreenadhchowdary <sreenadhchowdary@LAPTOP-0USD9F0J>
2021-12-01 14:53:33 +0530

    added newclass

---
+++ sample/src/com/lntinfotech/trainee/collections/NewClass.java
@@ -0,0 +1,10 @@
+package com.lntinfotech.trainee.collections;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a new class");
+	}
+
+}
